671b59c8be7355a778c66c2f91171ccecd14283b
Peter Grillot <petergrillot@gmail.com>
2018-01-25 15:56:44 -0500

    added api view

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+	
+	"""Test API View"""
+	def get(self, request, format=None):
+		
+		"""returns a List of APIViews"""
+		an_apiview = [
+			'Use HTTP methods (get post patch put delete)',
+			'Similar to Django',
+			'Manual URLS'
+			'Logic'
+		]
+
+		return Response({'mesage': 'Hello!', 'an_apiview': an_apiview})
